97c89d576357130cef9eaa26cc1240ba81a4ddd3
Alvin <alvinonline@pm.me>
2018-01-05 15:48:07 +0000

    added saveCanvas method, optimised saving

--- save.js
+++ save.js
@@ -5,6 +5,12 @@ document.body.appendChild(link); // Firefox workaround, see #6594
 const saveBlob = (blob, filename) => {
   link.href = URL.createObjectURL(blob);
   link.download = filename;
+  link.onclick = () => {
+    requestAnimationFrame(() => {
+      URL.revokeObjectURL(blob);
+      link.removeAttribute('href');
+    });
+  };
   link.click();
 };
 
@@ -18,3 +24,19 @@ export const saveBinary = (data, filename) => {
   const blob = new Blob([data], { type: 'application/octet-stream' });
   saveBlob(blob, filename);
 };
+
+export const saveCanvas = (canvas, filename) => {
+  const dataURL = canvas.toDataURL('image/png');
+  const blob = dataURLToBlob(dataURL);
+  saveBlob(blob, filename);
+};
+
+function dataURLToBlob(dataURL) {
+  const binStr = window.atob(dataURL.split(',')[1]);
+  const len = binStr.length;
+  const arr = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    arr[i] = binStr.charCodeAt(i);
+  }
+  return new Blob([arr]);
+}
